ad2be054255c3e60a74aac42b6a4c014d0bef6e9
Pedro Siqueira <pedrowss@gmail.com>
2014-06-19 15:10:23 -0300

    Refatora init.el

--- emacs/init.el
+++ emacs/init.el
@@ -2,28 +2,19 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
-(when (window-system)
-  (scroll-bar-mode -1)
-  (tool-bar-mode -1))
+;; ==================================================
+;;                 Basic Settings
+;; ==================================================
 
+(set-frame-font "Source Code Pro for Powerline-14")
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier nil)
 (setq ring-bell-function 'ignore)
 (setq default-directory "~/")
 
-(set-frame-font "Source Code Pro for Powerline-14")
-
-(global-set-key (kbd "RET") 'newline-and-indent)
-
-(global-set-key (kbd "M-k") 'next-line)
-(global-set-key (kbd "M-i") 'previous-line)
-(global-set-key (kbd "M-j") 'backward-char)
-(global-set-key (kbd "M-l") 'forward-char)
-
 (global-linum-mode t)
 (global-hl-line-mode 1)
 (global-visual-line-mode t)
-
 (delete-selection-mode t)
 (blink-cursor-mode t)
 (show-paren-mode t)
@@ -34,11 +25,125 @@
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
 (fset 'yes-or-no-p 'y-or-n-p)
+(electric-indent-mode t)
 
-(global-set-key (kbd "C-o") 'er/expand-region)
-(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+(when (window-system)
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1))
 
+;; ==================================================
+;;               AUTO MODES
+;; ==================================================
+
+;; Ruby
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("config.ru\\'" . ruby-mode))
+
+;; Web-mode
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+
+;; ORG mode
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . org-mode))
+
+;; ==================================================
+;;             PEDRO MODE MAPPINGS
+;; ==================================================
+
+;; Define my own keymap
+(defvar pedro-mode-map (make-keymap) "my keys")
+
+;; Cursor keys on home row
+(define-key pedro-mode-map (kbd "M-k") 'next-line)
+(define-key pedro-mode-map (kbd "M-i") 'previous-line)
+(define-key pedro-mode-map (kbd "M-j") 'backward-char)
+(define-key pedro-mode-map (kbd "M-l") 'forward-char)
+
+;; EXPAND REGION
+(define-key pedro-mode-map (kbd "C-o") 'er/expand-region)
+
+;; ACE JUMP MODE
+(define-key pedro-mode-map (kbd "C-c SPC") 'ace-jump-mode)
+
+;; CUSTOM FUNCTIONS
+(define-key pedro-mode-map (kbd "M-l") 'select-current-line)
+(define-key pedro-mode-map (kbd "M-RET") 'line-above)
+(define-key pedro-mode-map (kbd "C-M-y") 'duplicate-current-line-or-region)
+(define-key pedro-mode-map (kbd "C-c r") 'rename-this-buffer-and-file)
+
+
+;; ==================================================
+;;              PLUGINS and PACKAGES
+;; ==================================================
+
+;; IDO MODE
 (ido-mode 1)
 (ido-everywhere 1)
 (flx-ido-mode 1)
 (setq ido-use-faces nil)
+
+
+;; ==================================================
+;;              CUSTOM FUNCTIONS
+;; ==================================================
+
+(defun select-current-line ()
+  "Selects the current line"
+  (interactive)
+  (end-of-line)
+  (push-mark (line-beginning-position) nil t))
+
+(defun line-above()
+  "Inserts line above current one"
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline-and-indent)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(defun duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+If there's no region, the current line will be duplicated. However, if
+there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (let (beg end (origin (point)))
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        ;;(beginning-of-visual-line)
+        (insert region)
+        (indent-according-to-mode)
+        (setq end (point)))
+      (goto-char (+ origin (* (length region) arg) arg)))))
+
+(defun rename-this-buffer-and-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
+
+;; Define my own minor mode and activate it
+(define-minor-mode pedro-mode
+  "A minor mode for my custom keys and functions"
+  t " pedro" 'pedro-mode-map)
+(pedro-mode t)
