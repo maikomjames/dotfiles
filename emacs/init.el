93c14b63f5a4ce1509554b3b87076ad58f2854ff
Pedro Siqueira <pedrowss@gmail.com>
2014-08-11 17:50:12 -0300

    misterioso theme

--- emacs/init.el
+++ emacs/init.el
@@ -274,16 +274,5 @@ there's a region, all lines that region covers will be duplicated."
 ;; ==================================================
 
 (powerline-default-theme)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
- '(custom-safe-themes (quote ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(load-theme 'misterioso t)
